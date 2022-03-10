e8a720d3846303236abe8d65f5f0e9f02e8aa998
Diana Viglucci <dvigl86@gmail.com>
2022-03-10 16:29:13 -0500

    starting point before exploring NYC Open Data api

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,13 +1,15 @@
 import React from "react";
 import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
-
+import ratIcon from '../rat-icon.png';
 
 const dummyData = [
-    {id: 1, lat: 40.667660, lng: -73.961880},
-    {id: 2, lat: 40.741130, lng: -73.989690},
-    {id: 3, lat: 40.816971, lng: -73.924110},
-]
+  { id: 1, lat: 40.66766, lng: -73.96188 },
+  { id: 2, lat: 40.74113, lng: -73.98969 },
+  { id: 3, lat: 40.816971, lng: -73.92411 },
+  { id: 4, lat: 40.72402, lng: -73.95066 },
+];
 
+const icon = ratIcon;
 
 const key = process.env.REACT_APP_GOOGLE_MAPS_API_KEY;
 
@@ -17,7 +19,7 @@ const containerStyle = {
 };
 const center = {
   lat: 40.7128,
-  lng: -74.0060,
+  lng: -74.006,
 };
 
 function Map() {
@@ -32,23 +34,21 @@ function Map() {
     // wrapping to a function is useful in case you want to access `window.google`
     // to eg. setup options or create latLng object, it won't be available otherwise
     // feel free to render directly if you don't need that
-   
+
     return (
-        <div>
-          <GoogleMap
-        mapContainerStyle={containerStyle}
-        center={center}
-        zoom={11}
-      >
-          <Marker key='marker-1'  position={{lat: 40.724020, lng: -73.950660}}/>
+      <div>
+        <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={11}>
           {dummyData.map((element) => {
-              return (
-                  <Marker key={element.id} position={{lat: element.lat, lng: element.lng}}/>
-              )
+            return (
+              <Marker
+                key={element.id}
+                icon={icon}
+                position={{ lat: element.lat, lng: element.lng }}
+              />
+            );
           })}
-     </GoogleMap>
-      
-        </div>
+        </GoogleMap>
+      </div>
     );
   };
 
