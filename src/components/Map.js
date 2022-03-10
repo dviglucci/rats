5193030f92711cde4040226decba6733fc41d78f
Diana Viglucci <dvigl86@gmail.com>
2022-03-10 13:33:14 -0500

    misc code cleanup

--- src/components/Map.js
+++ src/components/Map.js
@@ -2,15 +2,6 @@ import React from "react";
 import { GoogleMap, useLoadScript } from "@react-google-maps/api";
 
 const key = process.env.REACT_APP_GOOGLE_MAPS_API_KEY;
-console.log("KEY ......", key)
-
-const options = {
-  // zoomControlOptions: {
-  //   position: google.maps.ControlPosition.RIGHT_CENTER // ,
-  //   // ...otherOptions
-  // }
-  key: key,
-};
 
 const containerStyle = {
   width: "100vw",
@@ -26,8 +17,6 @@ function Map() {
     googleMapsApiKey: key,
   });
 
-console.log('KEY line 28 >>>>>', key)
-
   if (loadError) return "Error loading maps";
   if (!isLoaded) return "Loading maps";
 
@@ -35,34 +24,15 @@ console.log('KEY line 28 >>>>>', key)
     // wrapping to a function is useful in case you want to access `window.google`
     // to eg. setup options or create latLng object, it won't be available otherwise
     // feel free to render directly if you don't need that
-    //   const onLoad = React.useCallback(
-    //     function onLoad (mapInstance) {
-    //       // do something with map Instance
-    //     }
-    //   )
-    //   return <GoogleMap
-    //     options={options}
-    //     onLoad={onLoad}
-    //   >
-    //     {
-    //       // ...Your map components
-    //     }
-    //   </GoogleMap>
-    console.log('KEY >>>>>>>>>', key)
+   
     return (
       <GoogleMap
         mapContainerStyle={containerStyle}
         center={center}
-        zoom={10}
+        zoom={11}
       ></GoogleMap>
     );
   };
-
-  // if (loadError) {
-  //   return <div>Map cannot be loaded right now, sorry.</div>
-  // }
-
-  // return isLoaded ? renderMap() : <Spinner />
 
   return renderMap();
 }
