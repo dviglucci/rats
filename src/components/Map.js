95ff97d2ed6953a048b1ba7ecff280468946fe95
Diana Viglucci <dvigl86@gmail.com>
2022-03-13 16:41:54 -0400

    can compute count of rats and pigeons in circle

--- src/components/Map.js
+++ src/components/Map.js
@@ -10,16 +10,38 @@ import pigeonIcon from "../pigeon-28px.png";
 import pizza from "../pizza.png";
 import { connect } from "react-redux";
 import axios from "axios";
+import google, { computeDistanceBetween } from "@googlemaps/react-wrapper";
 
 // const testStart = 2011;
 // const testEnd = 2022;
 
-let ratCount = 0;
-let pigeonCount = 0;
+let ratCount;
+let pigeonCount;
 let icon;
 
 // const rat = ratIcon;
 // const pigeon = pigeonIcon;
+
+
+////////////////////////////////////////
+
+var rad = function(x) {
+    return x * Math.PI / 180;
+  };
+  
+  var getDistance = function(p1, p2) {
+    var R = 6378137; // Earth’s mean radius in meter
+    var dLat = rad(p2.lat - p1.lat);
+    var dLong = rad(p2.lng - p1.lng);
+    var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+      Math.cos(rad(p1.lat)) * Math.cos(rad(p2.lat)) *
+      Math.sin(dLong / 2) * Math.sin(dLong / 2);
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    var d = R * c;
+    return d; // returns the distance in meter
+  };
+////////////////////////////////////////
+
 
 const containerStyle = {
   width: "80vw",
@@ -51,11 +73,8 @@ function Map(props) {
   const [items, setItems] = useState([]);
   const [circleCenter, setCircleCenter] = useState(startingCenter);
 
-  //   const {} = useLoadScript({
-  //     googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
-  //   });
   useLoadScript({
-    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY + '&libraries=geometry',
   });
 
   // The empty deps array [] means this useEffect will run once
@@ -95,8 +114,8 @@ function Map(props) {
       </div>
     );
   } else {
-    console.log("ITEMS>>>>>", items.length);
-    console.log(props);
+    ratCount = 0;
+    pigeonCount = 0;
     return (
       <div>
         <GoogleMap
@@ -117,12 +136,28 @@ function Map(props) {
             })
             .map((element) => {
               if (element.descriptor === "Rat Sighting") {
-                ratCount += 1;
                 icon = ratIcon;
+                const distanceToCenter = getDistance(
+                    circleCenter,
+                    {lat: parseFloat(element.latitude), lng: parseFloat(element.longitude)}
+                );
+                console.log("RAT DIST >>>>>", distanceToCenter)
+                if (distanceToCenter <= 1609.34) {
+                    ratCount += 1;
+                    console.log('RAT COUNT INCREMENTED >>>>', ratCount);
+                };
               } else {
-                pigeonCount += 1;
                 icon = pigeonIcon;
-              }
+                const distanceToCenter = getDistance(
+                    circleCenter,
+                    {lat: parseFloat(element.latitude), lng: parseFloat(element.longitude)}
+                );
+                console.log("PIGEON DIST >>>>>", distanceToCenter)
+                if (distanceToCenter <= 1609.34) {
+                    pigeonCount += 1;
+                    console.log('PIGEON COUNT INCREMENTED >>>>', pigeonCount);
+                };
+              };
               return (
                 <Marker
                   key={element.unique_key}
@@ -143,7 +178,6 @@ function Map(props) {
               onDrag={onDrag}
             />
           ) : null}
-          {console.log("circleCenter >>>", circleCenter)}
         </GoogleMap>
       </div>
     );
